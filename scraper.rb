d89449bff4afa59195a92eb5176a4b040e5e7a2c
Steven Noble <steven@snoble.net>
2016-03-14 21:23:02 +1100

    grab the member's electorate and party

--- scraper.rb
+++ scraper.rb
@@ -31,7 +31,9 @@ url = 'https://morph.io/documentation/examples/australian_members_of_parliament'
 page = agent.get(url)
 
 member = {
-  title: page.at('.search-filter-results').at('li').at('.title').inner_text.strip
+  title: page.at('.search-filter-results').at('li').at('.title').inner_text.strip,
+  electorate: page.at('.search-filter-results').at('li').search('dd')[0].inner_text,
+  party: page.at('.search-filter-results').at('li').search('dd')[1].inner_text
 }
 
 p member
