0894dd17704fa6dfd20d7e2e994df466eae18fb0
Steven Noble <steven@snoble.net>
2016-03-14 21:30:55 +1100

    get the information from all pages

--- scraper.rb
+++ scraper.rb
@@ -28,16 +28,18 @@ require 'mechanize'
 
 agent = Mechanize.new
 url = 'https://morph.io/documentation/examples/australian_members_of_parliament'
-page = agent.get(url)
 
-page.at('.search-filter-results').search('li').each do |li|
-  member = {
-    title: li.at('.title').inner_text.strip,
-    electorate: li.search('dd')[0].inner_text,
-    party: li.search('dd')[1].inner_text,
-    url: li.at('.title a').attr('href')
-  }
+["1", "2", "3"].each do |page_number|
+  page = agent.get(url + "?page=" + page_number)
 
-  p member
+  page.at('.search-filter-results').search('li').each do |li|
+    member = {
+      title: li.at('.title').inner_text.strip,
+      electorate: li.search('dd')[0].inner_text,
+      party: li.search('dd')[1].inner_text,
+      url: li.at('.title a').attr('href')
+    }
 
+    p member
+  end
 end
