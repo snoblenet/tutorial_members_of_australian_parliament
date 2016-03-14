ffb4292959cf2d713286035f76389cd60d6de9df
Steven Noble <steven@snoble.net>
2016-03-14 21:24:21 +1100

    grab all members from one page

--- scraper.rb
+++ scraper.rb
@@ -30,10 +30,14 @@ agent = Mechanize.new
 url = 'https://morph.io/documentation/examples/australian_members_of_parliament'
 page = agent.get(url)
 
-member = {
-  title: page.at('.search-filter-results').at('li').at('.title').inner_text.strip,
-  electorate: page.at('.search-filter-results').at('li').search('dd')[0].inner_text,
-  party: page.at('.search-filter-results').at('li').search('dd')[1].inner_text
-}
+page.at('.search-filter-results').search('li').each do |li|
+  member = {
+    title: li.at('.title').inner_text.strip,
+    electorate: li.search('dd')[0].inner_text,
+    party: li.search('dd')[1].inner_text,
+    url: li.at('.title a').attr('href')
+  }
 
-p member
+  p member
+
+end
